4e36a4a160faf25fc03368456da9dbcc7339847a
qiuxiqila <447847609@qq.com>
2021-02-05 17:55:39 +0800

    grpc报错添加日志

--- src/Exception/Handler/GrpcExceptionHandler.php
+++ src/Exception/Handler/GrpcExceptionHandler.php
@@ -10,10 +10,11 @@ namespace Chive\Exception\Handler;
 
 
 use Chive\Exception\BusinessException;
-use Chive\Helper\ErrorHelper;
+use Chive\Helper\GrpcErrorHelper;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\Logger\LoggerFactory;
 use Hyperf\Validation\ValidationException;
 use Pb\Reply;
 use Psr\Http\Message\ResponseInterface;
@@ -23,50 +24,59 @@ use Hyperf\Grpc\Parser;
 
 class GrpcExceptionHandler extends ExceptionHandler
 {
-    /**
-     * @var StdoutLoggerInterface
-     */
-    protected $logger;
+	/**
+	 * @var LoggerInterface
+	 */
+	protected $logger;
 
-    public function __construct(StdoutLoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
+	/**
+	 * @var StdoutLoggerInterface
+	 */
+	protected $stdoutLogger;
 
-    public function handle(Throwable $throwable, ResponseInterface $response)
-    {
-        $message = new Reply();
-        $message->setErrCode(ErrorHelper::FAIL_CODE);
-        $message->setMsg('网络错误');
-        if ($throwable instanceof BusinessException || $throwable instanceof ValidationException) {
-            $message->setErrCode($throwable->getCode());
-            $message->setMsg($throwable->getMessage());
-            return $this->handleResponse($message, $response);
-        }
+	public function __construct(StdoutLoggerInterface $stdoutLogger, LoggerFactory $loggerFactory)
+	{
+		$this->stdoutLogger = $stdoutLogger;
+		$this->logger       = $loggerFactory->get('error', 'error');
+	}
 
-        $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+	public function handle(Throwable $throwable, ResponseInterface $response)
+	{
+		$message = new Reply();
+		$message->setErrCode(GrpcErrorHelper::FAIL_CODE);
+		$message->setMsg('网络错误');
+		if ($throwable instanceof BusinessException || $throwable instanceof ValidationException) {
+			$message->setErrCode($throwable->getCode());
+			$message->setMsg($throwable->getMessage());
+			$this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+			$this->stdoutLogger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+			return $this->handleResponse($message, $response);
+		}
 
-        //return $this->transferToResponse($throwable->getCode(), $throwable->getMessage(), $response);
-        return $this->handleResponse($message, $response);
-    }
+		$this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
+		$this->stdoutLogger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
 
-    public function isValid(Throwable $throwable): bool
-    {
-        return true;
-    }
+		//return $this->transferToResponse($throwable->getCode(), $throwable->getMessage(), $response);
+		return $this->handleResponse($message, $response);
+	}
 
-    /**
-     * Handle GRPC Response.
-     * @param int $httpStatus
-     */
-    protected function handleResponse(?Message $message, ResponseInterface $response, $httpStatus = 200, string $grpcStatus = '0', string $grpcMessage = ''): ResponseInterface
-    {
-        return $response->withStatus($httpStatus)
-            ->withBody(new SwooleStream(Parser::serializeMessage($message)))
-            ->withAddedHeader('Server', 'Hyperf')
-            ->withAddedHeader('Content-Type', 'application/grpc')
-            ->withAddedHeader('trailer', 'grpc-status, grpc-message')
-            ->withTrailer('grpc-status', $grpcStatus)
-            ->withTrailer('grpc-message', $grpcMessage);
-    }
+	public function isValid(Throwable $throwable): bool
+	{
+		return true;
+	}
+
+	/**
+	 * Handle GRPC Response.
+	 * @param int $httpStatus
+	 */
+	protected function handleResponse(?Message $message, ResponseInterface $response, $httpStatus = 200, string $grpcStatus = '0', string $grpcMessage = ''): ResponseInterface
+	{
+		return $response->withStatus($httpStatus)
+			->withBody(new SwooleStream(Parser::serializeMessage($message)))
+			->withAddedHeader('Server', 'Hyperf')
+			->withAddedHeader('Content-Type', 'application/grpc')
+			->withAddedHeader('trailer', 'grpc-status, grpc-message')
+			->withTrailer('grpc-status', $grpcStatus)
+			->withTrailer('grpc-message', $grpcMessage);
+	}
 }
