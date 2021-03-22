187de5d4da7130cab122aa66aec9a16d84c265f4
qiuxiqila <447847609@qq.com>
2021-03-22 14:58:09 +0800

    添加Yapi引用例子

---
+++ example/Command/YapiCommand.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class YapiCommand
+ * 作者: su
+ * 时间: 2021/3/22 14:55
+ * 备注:
+ */
+
+namespace App\Command;
+
+use Hyperf\Command\Annotation\Command;
+
+/**
+ * @Command
+ */
+class YapiCommand extends \Chive\Command\YapiCommand
+{
+
+}
